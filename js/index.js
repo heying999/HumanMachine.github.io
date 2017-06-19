0551262710cccd89554898ca168090867051303e
Ying He <heying@naneis-MBP.fios-router.home>
2017-06-19 16:06:42 -0400

    HumanMachine

---
+++ js/index.js
@@ -0,0 +1,111 @@
+var doc = document,
+	win = window,
+	body = doc.body;
+
+var ww = win.innerWidth,
+	wh = win.innerHeight;
+
+var c = doc.createElement('canvas'),
+	ctx = c.getContext('2d');
+
+var half_PI = Math.PI / 2,
+	two_PI = Math.PI * 2,
+	ease = 0.01;
+
+
+// var k = {
+// 	offsetRotation: 0,
+// 	offsetScale: .8,
+// 	offsetX: 0,
+// 	offsetY: 0,
+// 	radius: 1100,
+// 	slices: 16,
+// 	zoom: 1.5
+// };
+var k = {
+	offsetRotation: 180,
+	offsetScale: .39,
+	offsetX: 0,
+	offsetY: 0,
+	radius: 999,
+	slices: 5.9,
+	zoom: 1
+};
+
+
+body.appendChild(c);
+c.width = k.radius * 2;
+c.height = k.radius * 2;
+
+var img = new Image();
+ img.src = 'https://lh5.googleusercontent.com/eBEmDWsq3ijhp7_k9TQZQ75wa0WH7O2MUfucBdH3GrFlNlYHnjdJCQTSzwCln7VDNCl6edHjz8cWVkU=w2790-h1396-rw';
+var fill = ctx.createPattern(img, 'repeat');
+
+var scale, step, cx;
+
+scale = k.zoom * (k.radius / Math.min(img.width, img.height));
+step = two_PI / k.slices;
+cx = img.width / 2;
+
+function draw(){
+
+	ctx.fillStyle = fill;
+
+	for (var i = 0; i <= k.slices; i++) {
+		ctx.save();
+		ctx.translate(k.radius, k.radius);
+		ctx.rotate(i * step);
+		ctx.beginPath();
+		ctx.moveTo(-0.3, -0.3);
+		ctx.arc(0, 0, k.radius, step * -0.61, step * 0.61);
+		ctx.rotate(half_PI);
+		ctx.scale(scale, scale);
+		ctx.scale([ -1,1 ][i % 2], 1);
+		ctx.translate(k.offsetX - cx, k.offsetY);
+		ctx.rotate(k.offsetRotation);
+		ctx.scale(k.offsetScale, k.offsetScale);
+		ctx.fill();
+
+		ctx.restore();
+
+	}
+
+}
+
+
+var tx = k.offsetX;
+var ty = k.offsetY;
+var tr = k.offsetRotation;
+
+
+win.addEventListener('mousemove', mousemove, false);
+function mousemove(e){
+    var cx, cy, dx, dy, hx, hy;
+    cx = ww / 2;
+    cy = wh / 2;
+    dx = e.pageX / ww;
+    dy = e.pageY / wh;
+    hx = dx - 0.1;
+    hy = dy - 0.1;
+    tx = hx * k.radius * -.8;
+    ty = hy * k.radius * .8;
+}
+
+c.style.position = 'fixed';
+c.style.marginLeft = -k.radius + 'px';
+c.style.marginTop = -k.radius + 'px';
+c.style.left = '50%';
+c.style.top = '50%';
+
+function update() {
+    tr -= 0.002;
+
+    k.offsetX += (tx - k.offsetX) * ease;
+    k.offsetY += (ty - k.offsetY) * ease;
+    k.offsetRotation += (tr - k.offsetRotation) * ease;
+
+	draw();
+
+    requestAnimationFrame(update);
+};
+update();
